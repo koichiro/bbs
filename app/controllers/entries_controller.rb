1320c1b6d6942e8adf23ec79ff98fe7147801ab9
Koichiro Ohba <koichiro@meadowy.org>
2011-12-17 16:36:36 +0900

    implement create action

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -1,5 +1,15 @@
 class EntriesController < ApplicationController
   def index
+    @entry = Entry.new
+  end
+  
+  def create
+    @entry = Entry.new(params[:entry])
+    if @entry.save
+      redirect_to entries_path
+    else
+      render :index
+    end
   end
 
 end
